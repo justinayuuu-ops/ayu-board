852f017d728ce4642e06b856003795be8000e5e2
justin <justin.nn@kakaocorp.com>
2026-09-26 20:54:21 +0900

    Revert "앱 제목 옆 아이콘은 다시 로켓 (홈 화면 아이콘은 아유 유지)"
    This reverts commit 4d3173f51f4662d6dd69389be96c2ec3907523f6.

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    아이콘·글꼴은 캐시 우선: 인터넷이 없어도 앱이 그대로 뜬다.
    학습 기록은 캐시가 아니라 localStorage 에 있으므로 캐시를 비워도 사라지지 않는다. */
 
-var VERSION = "ayuboard-2026-09-26p";
+var VERSION = "ayuboard-2026-09-26o";
 var PREFIX = "ayuboard-";
 var SHELL = PREFIX + "shell-" + VERSION;
 
@@ -19,6 +19,7 @@ var SHELL_FILES = [
   "./icon-512-r4.png",
   "./icon-maskable-512-r4.png",
   "./favicon-48-r4.png",
+  "./ayu-avatar-r1.png",
   "./Jua-Korean.woff2",
   "./KakaoSmallSans-Regular.woff2",
   "./KakaoSmallSans-Bold.woff2"
